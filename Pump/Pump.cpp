da549dee1e032dc4f70a4b58ef10f1832d25a708
Carrie Lai <carrie_k_y_lai@hotmail.com>
2012-11-03 15:00:24 -0700

    lowercased files

--- Pump/Pump.cpp
+++ Pump/Pump.cpp
@@ -1,5 +1,14 @@
 /*
- * Student Names:	Kathy Xu, Carrie Lai
+ * Student Names:  Kathy Xu, Carrie Lai
  * Student Numbers: 21859079, 61230180
- * Lab:				EECE 314 L1D
+ * Lab:        EECE 314 L1D
  */
+
+#include "rt.h"
+#include "Pump.h"
+
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
--- Pump/Pump.h
+++ Pump/Pump.h
@@ -1,5 +1,21 @@
 /*
- * Student Names:	Kathy Xu, Carrie Lai
+ * Student Names:  Kathy Xu, Carrie Lai
  * Student Numbers: 21859079, 61230180
- * Lab:				EECE 314 L1D
+ * Lab:        EECE 314 L1D
  */
+
+#include "rt.h"
+
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+
+class Pump : public ActiveClass {
+  public:
+
+
+  private: 
+
+};
